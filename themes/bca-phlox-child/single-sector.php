607938c2ad26d1b686fee59ce95b8f2939ac3772
deven22-ai <deven@aishatech.ai>
2026-03-31 10:48:35 +0100

    Added CTA shortcode and organised the code base

--- themes/bca-phlox-child/single-sector.php
+++ themes/bca-phlox-child/single-sector.php
@@ -1,4 +1,7 @@
-<?php get_header(); ?>
+<?php 
+get_header(); 
+wp_enqueue_style('sectors-style');
+?>
 
 <main class="bca-single-sector">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
